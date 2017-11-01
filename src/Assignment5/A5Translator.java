3d54f04584b2fd3a3877edcaee3b579ad378f903
wrigm7406 <wrigm7406@HRH-IC0027344.wrdsb.ca>
2017-11-01 14:00:13 -0400

    A5 Translator Complete

---
+++ src/Assignment5/A5Translator.java
@@ -0,0 +1,102 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author wrigm7406
+ */
+public class A5Translator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a scanner
+        Scanner input = new Scanner(System.in);
+        // ask for a word to translate to ubbi dubbi
+        System.out.println("Please enter a word to translate to Ubbi Dubbi:");
+        String wordUno = input.nextLine();
+        // convert word to lowercase
+        wordUno = wordUno.toLowerCase();
+        // create an empty string to later be replaced by the translated word
+        String trans = "";
+        // create a string with spaces so the translator doesn't brake
+        String wordDos = wordUno + "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ";
+        // if the word "exit" is typed, break the translator
+        String quit = "exit";
+
+        // find a vowel by going through each letter
+        for (int i = 0; i < wordDos.length(); i = i + 1) {
+            if (wordDos.equals(quit)) {
+                break;
+            }
+
+            // search for vowels in the word, if there is a vowel, perform multiple actions
+            if (wordDos.charAt(i) == 'a' || wordDos.charAt(i) == 'e' || wordDos.charAt(i) == 'i' || wordDos.charAt(i) == 'o' || wordDos.charAt(i) == 'u') {
+                // add the prefix "ub" before the vowel
+                trans = trans + "ub" + wordDos.charAt(i);
+                // if the vowel is 'a', check what the next letter to the right of it is
+                if (wordDos.charAt(i++) == 'a') {
+                    // leave the word how it originally is
+                    trans = trans + wordDos.charAt(i);
+                } else {
+                    // if a vowel cannot be found after the original vowel, continue through the word
+                    trans = trans + wordDos.charAt(i);
+                }
+            } else {
+                trans = trans + wordDos.charAt(i);
+            }
+
+            // search for vowels in the word, if there is a vowel, perform multiple actions
+            if (wordDos.charAt(i) == 'a' || wordDos.charAt(i) == 'e' || wordDos.charAt(i) == 'i' || wordDos.charAt(i) == 'o' || wordDos.charAt(i) == 'u') {
+                if (wordDos.charAt(i++) == 'e') {
+                    // if the vowel is 'a', check what the next letter to the right of it is
+                    trans = trans + wordDos.charAt(i);
+                } else {
+                    // if a vowel cannot be found after the original vowel, continue through the word
+                    trans = trans + wordDos.charAt(i);
+                }
+            }
+
+            // search for vowels in the word, if there is a vowel, perform multiple actions
+            if (wordDos.charAt(i) == 'a' || wordDos.charAt(i) == 'e' || wordDos.charAt(i) == 'i' || wordDos.charAt(i) == 'o' || wordDos.charAt(i) == 'u') {
+                if (wordDos.charAt(i++) == 'i') {
+                    // if the vowel is 'a', check what the next letter to the right of it is
+                    trans = trans + wordDos.charAt(i);
+                } else {
+                    // if a vowel cannot be found after the original vowel, continue through the word
+                    trans = trans + wordDos.charAt(i);
+                }
+            }
+
+            // search for vowels in the word, if there is a vowel, perform multiple actions
+            if (wordDos.charAt(i) == 'a' || wordDos.charAt(i) == 'e' || wordDos.charAt(i) == 'i' || wordDos.charAt(i) == 'o' || wordDos.charAt(i) == 'u') {
+                if (wordDos.charAt(i++) == 'o') {
+                    // if the vowel is 'a', check what the next letter to the right of it is
+                    trans = trans + wordDos.charAt(i);
+                } else {
+                    // if a vowel cannot be found after the original vowel, continue through the word
+                    trans = trans + wordDos.charAt(i);
+                }
+            }
+
+            // search for vowels in the word, if there is a vowel, perform multiple actions
+            if (wordDos.charAt(i) == 'a' || wordDos.charAt(i) == 'e' || wordDos.charAt(i) == 'i' || wordDos.charAt(i) == 'o' || wordDos.charAt(i) == 'u') {
+                if (wordDos.charAt(i++) == 'u') {
+                    // if the vowel is 'a', check what the next letter to the right of it is
+                    trans = trans + wordDos.charAt(i);
+                } else {
+                    // if a vowel cannot be found after the original vowel, continue through the word
+                    trans = trans + wordDos.charAt(i);
+                }
+            }
+        }
+        // tell the user their translated word
+        System.out.println("your word is " + trans);
+    }
+}
